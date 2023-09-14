6b82203ff51535ce2003741e49dbbfdb3d49147f
suryaKanthjunior <114057966+suryaKanthjunior@users.noreply.github.com>
2023-09-14 16:08:42 +0530

    committed

--- src/components/FormFile.tsx
+++ src/components/FormFile.tsx
@@ -1,19 +1,128 @@
 import { Box, Button, Stack, TextField, Typography } from "@mui/material";
 import FormAddress from "./FormAddress";
-import { useState } from "react";
-
-
+import { FormEvent, useCallback, useEffect, useState } from "react";
+import CustomTextField from "../CommonComponent/CustomTextField";
 
 export default function FormFile() {
+ 
 
-  const initialValues = { firstName: "", lastname: "", email: "", phone: ""}
+ const [firstname, setFirstName] = useState("");
+ const [lastName, setLastName] = useState("");
+ const [email, setEmail] = useState("");
+ const [phone, setPhone] = useState("");
+ const [firstnameError, setFirstNameError] = useState("");
+ const [lastnameError, setLastNameError] = useState("");
+ const [emailError, setEmailError] = useState("");
+ const [phoneError, setPhoneError] = useState("");
+ const [passage, setPassage] = useState("");
+ const [passageError, setPassageError] = useState("");
 
-  const [name, setName] = useState(initialValues);
 
-  const handleChange = () => {
-    setName(initialValues);
+ const PassageValidation = (name: string) => {
+  if (/^[a-zA-Z\s]{1,40}$/.test(name)) {
+    setPassageError("");
+    return true;
+  } else {
+    setPassageError("Only allowed characters and space");
+    return false;
   }
+};
 
+useEffect(() => {
+  if (passage) {
+    PassageValidation(passage);
+  }
+  {
+    setPassage(passage);
+  }
+}, [passage]);
+
+
+ const FirstNameValidation = (name: string) => {
+  if (/^[a-zA-Z\s]{1,40}$/.test(name)) {
+    setFirstNameError("");
+    return true;
+  } else {
+    setFirstNameError("Only allowed characters and space");
+    return false;
+  }
+};
+
+const LastNameValidation = (name: string) => {
+  if (/^[a-zA-Z\s]{1,40}$/.test(name)) {
+    setLastNameError("");
+    return true;
+  } else {
+    setLastNameError("Only allowed characters and space");
+    return false;
+  }
+};
+
+const EmailValidation = (name: string) => {
+  if (/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(name)) {
+    setEmailError("");
+    return true;
+  } else {
+    setEmailError("Please include an '@' in the email");
+    return false;
+  }
+};
+
+const PhoneValidation = (name: string) => {
+  if (/^\d{10}$/.test(name)) {
+    setPhoneError("");
+    return true;
+  } else {
+    setPhoneError("Phone number must have 10 digits");
+    return false;
+  }
+};
+
+useEffect(() => {
+  if (phone) {
+    PhoneValidation(phone);
+  }
+  {
+    setPhone(phone);
+  }
+}, [phone]);
+
+useEffect(() => {
+  if (email) {
+    EmailValidation(email);
+  }
+  {
+    setEmail(email);
+  }
+}, [email]);
+  
+
+
+
+useEffect(() => {
+  if (firstname) {
+    FirstNameValidation(firstname);
+  }
+  if (lastName) {
+    LastNameValidation(lastName);
+  }
+  {
+    setFirstName(firstname);
+    setLastName(lastName);
+  }
+}, [firstname, lastName]);
+  
+const handleSubmit = (e: any) => {
+  e.preventDefault()
+  const data = { 
+firstname: firstname,
+lastName: lastName,
+email: email,
+phone: phone,
+  }
+  
+  console.log(data);
+}
 
 
     return (
@@ -24,20 +133,22 @@ export default function FormFile() {
                    spacing={3}>
                      <Box>
                    <Typography marginBottom={"12px"} fontSize={"16px"} color="black" fontWeight={"bold"}>FIRST NAME</Typography>
-                <TextField  sx={{
+                <CustomTextField  required={true}  sx={{
               ".MuiOutlinedInput-input": {
               width: "300px"
               },
-            }} id="outlined-basic" value={initialValues.firstName} onChange={handleChange}  variant="outlined" placeholder="Enter your Name" /> 
+            }} id="outlined-basic"  error={firstname ? !!firstnameError : false}
+            helperText={firstnameError} value={firstname} onChange={setFirstName}  variant="outlined" placeholder="Enter your Name" /> 
             </Box>
             <Box>
               <Typography marginBottom={"12px"} fontSize={"16px"} color="black" fontWeight={"bold"}>LAST NAME</Typography>
             
-        <TextField  sx={{
+        <CustomTextField  sx={{
               ".MuiOutlinedInput-input": {
                 width: "300px"
               },
-            }} id="outlined-basic" value={initialValues.lastname} variant="outlined" placeholder="Enter your Name" /> 
+            }} id="outlined-basic" value={lastName} error={lastName ? !!lastnameError : false}
+            helperText={lastnameError} onChange={setLastName}  variant="outlined" placeholder="Enter your Name" /> 
             </Box>
             </Stack>
             <Stack margin={"30px"} direction="row"
@@ -46,36 +157,39 @@ export default function FormFile() {
                      <Box>
                    <Typography marginBottom={"12px"} fontSize={"16px"} color="black" fontWeight={"bold"}>EMAIL ID</Typography>
                   
-                <TextField  sx={{
+                <CustomTextField  sx={{
               ".MuiOutlinedInput-input": {
               width: "300px"
               },
-            }} id="outlined-basic" value={initialValues.email} variant="outlined" placeholder="Enter your Email ID" /> 
+            }} id="outlined-basic" error={email ? !!emailError : false}
+            helperText={emailError} value={email} onChange={setEmail}  variant="outlined" placeholder="Enter your Email ID" /> 
             </Box>
             <Box>
               <Typography marginBottom={"12px"} fontSize={"16px"} color="black" fontWeight={"bold"}>PHONE NUMBER</Typography>
-        <TextField  sx={{
+        <CustomTextField  sx={{
               ".MuiOutlinedInput-input": {
                 width: "300px"
               },
-            }} id="outlined-basic" value={initialValues.phone} type="number" variant="outlined" placeholder="Enter your Phone No" /> 
+            }} id="outlined-basic" type="number" error={phone ? !!phoneError : false}
+            helperText={phoneError} value={phone} onChange={setPhone} variant="outlined" placeholder="Enter your Phone No" /> 
             </Box>
             </Stack>
              <Box margin={"30px"}>
               <Typography margin={"0px 8px"} marginBottom={"12px"} fontSize={"16px"} color="black" fontWeight={"bold"}>WHAT DO YOU HAVE IN MIND</Typography>
             
-        <TextField   sx={{
+        <CustomTextField   sx={{
            
               ".MuiOutlinedInput-input": {
                 width: "650px",
                 marginBottom: "130px"
               },
-            }} id="outlined-basic" variant="outlined" placeholder="Please enter query..." /> 
+            }} id="outlined-basic" value={passage} onChange={setPassage} error={passage ? !!passageError : false}
+            helperText={passageError}  variant="outlined" placeholder="Please enter query..." /> 
             </Box>
             <Box margin={"0px 290px"}>
             <Button sx={{
                 padding: "7px 50px"
-            }} variant="contained">Submit</Button>
+            }} variant="contained" onClick={handleSubmit}>Submit</Button>
             </Box>
         </Box> 
         <FormAddress />
