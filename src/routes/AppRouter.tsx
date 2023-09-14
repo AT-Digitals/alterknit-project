f3b1543b597dde8e5a19960fc7c7cdc9700e0a9e
Dinesh <djeeva1515@gmail.com>
2023-09-14 19:14:34 +0530

    registration authentication changes

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,22 +1,19 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
 import AlterknitHeader from "../AppHeader/AlterknitHeader";
-import ResgidterEmail from "../LoginDetails/ResgisterEmail";
-import routes from "./routes";
 import LoginPage from "../Login-details/LoginPage";
 import SignUpPage from "../Register/SignUpPage";
+import routes from "./routes";
 
 export default function AppRouter() {
-    return (
-        <Router>
-            <Routes>
-                <Route path={routes.LOGIN} element={<LoginPage />} />
-                <Route path={routes.LOGOUT} />
-                <Route path={routes.ALTERKNIT_HEADER} element={<AlterknitHeader />} />
-                <Route path={routes.REGISTER_EMAIL} element={<ResgidterEmail />} />
-                <Route path={routes.SIGNUP} element={<SignUpPage />} />
-            </Routes>
-        </Router>
-    );
-
+  return (
+    <Router>
+      <Routes>
+        <Route path={routes.LOGIN} element={<LoginPage />} />
+        <Route path={routes.LOGOUT} />
+        <Route path={routes.ALTERKNIT_HEADER} element={<AlterknitHeader />} />
+        <Route path={routes.SIGNUP} element={<SignUpPage />} />
+      </Routes>
+    </Router>
+  );
 }
