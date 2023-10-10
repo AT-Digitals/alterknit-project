90350d4c8c377d55d5ece2a5ebc1f15110142845
Dinesh <djeeva1515@gmail.com>
2023-10-10 16:13:40 +0530

    button changes fixed

--- src/Popup/Popup.tsx
+++ src/Popup/Popup.tsx
@@ -1,4 +1,4 @@
-import { Box, DialogContent, DialogTitle } from "@mui/material";
+import { Box, DialogContent, DialogTitle, styled } from "@mui/material";
 
 import CustomButton from "../CommonComponent/CustomButton";
 import Dialog from "@mui/material/Dialog";
@@ -11,6 +11,17 @@ interface ModalProps {
   message: string;
 }
 
+const StyledButton = styled(CustomButton)({
+  borderRadius: "5px",
+  fontSize: "22px",
+  fontWeight: 600,
+  // padding: "16px 30px",
+  textTransform: "none",
+  whiteSpace: "nowrap",
+  fontFamily: `"ProximaNovaSemibold", sans-serif !important`,
+  backgroundColor: "black",
+});
+
 export default function CustomDialog({ isOpen, onClose, message }: ModalProps) {
   return (
     <Dialog open={isOpen} onClose={onClose}>
@@ -19,7 +30,9 @@ export default function CustomDialog({ isOpen, onClose, message }: ModalProps) {
           <img width={100} src={alter} alt="alter" />
         </DialogTitle>
         <DialogContent> {message} </DialogContent>
-        <CustomButton onClick={onClose}>Close</CustomButton>
+        <StyledButton bgColor="black" onClick={onClose}>
+          Close
+        </StyledButton>
       </Box>
     </Dialog>
   );
